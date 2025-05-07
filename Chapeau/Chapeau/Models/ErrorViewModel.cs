7c74f3ed00ceb19f831976e2d4f397e27f3e7757
Jeroen-school <731141@student.inholland.nl>
2025-05-07 21:54:45 +0200

    Chapeau project
    First upload, no content yet

---
+++ Chapeau/Chapeau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chapeau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
